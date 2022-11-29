88f4cb3c03346d3461ba53d066c01f0165076bed
Gregory Wainer <greg.wainer@gmail.com>
2022-11-29 13:28:33 -0500

    Fixed Async and added user input loop

--- HW13.py
+++ HW13.py
@@ -1,53 +1,65 @@
 import hashlib, binascii
 import time
+import asyncio
 
-start = time.time()
-dict_array = []
-dict_file = open("common-passwords-win.txt", "r")
+def make_dictionary(file):
+    dict_array = []
+    dict_file = open(file, "r")
 
-for line in dict_file:
-  dict_array.append(line.rstrip('\n'))
-  # rstrip('\n') removes newline characters from the words, you might not need this.
+    for line in dict_file:
+        dict_array.append(line.rstrip('\n'))
+    
+    return dict_array
 
-password = input("Enter password: ")
+async def hash256(password):
+    hashed_pwd = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), 'saltPhrase'.encode('utf-8'), 100000) 
+    return binascii.hexlify(hashed_pwd)
 
-def hash256(password):
-    hashed_pwd = hashlib.pbkdf2_hmac('sha256', #later you will changes this to sha512
-                         password.encode('utf-8'),# password is encoded into binary
-                         'saltPhrase'.encode('utf-8'),# 'salt' is an extra bit of 
-#info added to the password. When using randomized 'salt' dictionary attacks becomes
-#nealry impossible. For this project keep 'salt' static. In the real world 'salt' is
-#randomized and later exctracted from the hashed password during the verififcation 
-#process. Essentially, the 'salt' portion of the hash can be separated from the 
-#password portion.
-                         100000) # number of iterations ('resolution') of the hashing computation)
-    return binascii.hexlify(hashed_pwd)# converting binary to hex
-
-def hash512(password):
+async def hash512(password):
     hashed_pwd = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), 'saltPhrase'.encode('utf-8'), 100000)
     return binascii.hexlify(hashed_pwd)
 
-hashed_pwd_hex_256 = hash256(password)
-hashed_pwd_hex_512 = hash512(password)
+async def main():
+    dict_array = make_dictionary("common-passwords-win.txt")
 
-print("\t256 SHA: ", hashed_pwd_hex_256)
-print("\t512 SHA: ", hashed_pwd_hex_512)
+    hashes = await asyncio.gather(
+        hash256(password),
+        hash512(password)
+    )
+
+    print("\t256 SHA: ", hashes[0].decode("utf-8") )
+    print("\t512 SHA: ", hashes[1].decode("utf-8"), "\n")
+
+    cracked_words = await asyncio.gather(
+        crack_array(hashes[0], hashes[1], dict_array)
+    )
 
 
-# print("Pwd is:" + password + " Length is: ", len(password))
+async def crack_array(hashed_pwd_hex_256, hashed_pwd_hex_512, dict_array):
+    start256 = time.time()
+    for word in dict_array:
+        hash256_word = await hash256(word)
+        if hash256_word == hashed_pwd_hex_256:
+            print("Cracked SHA256: " + word)
+            print("Time to crack: ", time.time() - start256, "\n")
+            break
 
-# print(dict_array)
+    start512 = time.time()
+    for word in dict_array:
+        hash512_word = await hash512(word)
+        if hash512_word == hashed_pwd_hex_512:
+            print("Cracked SHA512: " + word)
+            print("Time to crack: ", time.time() - start512, "\n")
+            break
 
-for word in dict_array:
-    if hash256(word) == hashed_pwd_hex_256:
-        print("Cracked SHA256: " + word)
-        print("Time to crack: ", time.time() - start)
-        break
+start = time.time()
 
-for word in dict_array:
-    if hash512(word) == hashed_pwd_hex_512:
-        print("Cracked SHA512: " + word)
-        print("Time to crack: ", time.time() - start)
-        break
-
-print("Elpased time: ", (time.time() - start), " seconds.")
+if __name__ == '__main__':
+    while (True):
+        password = input("Enter password: ")
+        while (password != 'q'):
+            asyncio.run(main())
+            password = input("Enter password: ")
+        else:
+            print("Elpased time: ", (time.time() - start), " seconds.")
+            break
