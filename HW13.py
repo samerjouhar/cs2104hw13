b468903b400fc8cbd17ee3ae5211c23de8da1d64
Gregory Wainer <greg.wainer@gmail.com>
2022-12-01 09:10:29 -0500

    recursion

--- HW13.py
+++ HW13.py
@@ -1,7 +1,7 @@
 import hashlib, binascii
 import time
 import asyncio
-import matplotlib, numpy
+import matplotlib, numpy, itertools
 
 def make_dictionary(file):
     dict_array = []
@@ -12,11 +12,11 @@ def make_dictionary(file):
     
     return dict_array
 
-async def hash256(password):
+async def hash256(password: str):
     hashed_pwd = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), 'saltPhrase'.encode('utf-8'), 100000) 
     return binascii.hexlify(hashed_pwd)
 
-async def hash512(password):
+async def hash512(password: str):
     hashed_pwd = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), 'saltPhrase'.encode('utf-8'), 100000)
     return binascii.hexlify(hashed_pwd)
 
@@ -24,6 +24,7 @@ async def main():
     #dict_array = make_dictionary("common-passwords-win.txt")
     #dict_array = make_dictionary("best1050.txt")
     dict_array = make_dictionary("common-full")
+    all_words = itertools.permutations(dict_array)
 
     hashes = await asyncio.gather(
         hash256(password),
@@ -33,22 +34,36 @@ async def main():
     print("\t256 SHA: ", hashes[0].decode("utf-8") )
     print("\t512 SHA: ", hashes[1].decode("utf-8"), "\n")
 
+    layer = 1
+    word = ""
     cracked_words = await asyncio.gather(
-        crack_array(hashes[0], hashes[1], dict_array)
+        crack_array(hashes[0], hashes[1], all_words, layer, word)
     )
 
 
-async def crack_array(hashed_pwd_hex_256, hashed_pwd_hex_512, dict_array):
+async def crack_array(hashed_pwd_hex_256: bytes, hashed_pwd_hex_512: bytes, all_words, layer: int, word: str):
     start256 = time.time()
-    found256 = False
-    for word in dict_array:
-        hash256_word = await hash256(word)
+
+    
+    
+    
+    await crack_array(hashed_pwd_hex_256, hashed_pwd_hex_512, all_words, layer + 1, word)
+
+
+
+
+
+    for index in range(len(dict_array)):
+        word += dict_array[index]
+        hash256_word = await hash256(dict_array[index + word_length])
         if hash256_word == hashed_pwd_hex_256:
-            print("Cracked SHA256: " + word)
+            print("Cracked SHA256: " + dict_array[index])
             print("Time to crack: ", time.time() - start256, "\n")
             found256 = True
             break
+
     if (not found256):
+        await crack_array(hash256(word), hashed_pwd_hex_512, dict_array, word_length + 1)
         print("No SHA256 Passwords Cracked")
 
     start512 = time.time()
