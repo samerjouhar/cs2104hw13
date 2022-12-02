03905f6fd3c8ce2f6f04901dc191c231f1a8ed84
Gregory Wainer <greg.wainer@gmail.com>
2022-12-02 12:55:18 -0500

    changed some stuff but long words still don't work
    Signed-off-by: Gregory Wainer <greg.wainer@gmail.com>

--- HW13.py
+++ HW13.py
@@ -1,15 +1,12 @@
-import asyncio
 import binascii
 import hashlib
 import time
-import sys
-import concurrent.futures
-from multiprocessing import cpu_count
+from multiprocessing import Process
 from itertools import permutations
 
 import matplotlib.pyplot as plt
 
-def make_dictionary(file):
+def make_dictionary(file) -> list[str]:
     dict_array = []
     dict_file = open(file, "r")
 
@@ -18,91 +15,80 @@ def make_dictionary(file):
     
     return dict_array
 
-async def hash256(password: str):
+def hash256(password: str):
     hashed_pwd = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), 'saltPhrase'.encode('utf-8'), 100000) 
     return binascii.hexlify(hashed_pwd)
 
-async def hash512(password: str):
+def hash512(password: str):
     hashed_pwd = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), 'saltPhrase'.encode('utf-8'), 100000)
     return binascii.hexlify(hashed_pwd)
 
-async def main():
+def main():
     #dict_array = make_dictionary("common-passwords-win.txt")
     #dict_array = make_dictionary("best1050.txt")
     dict_array = make_dictionary("common-full")
-    NUM_CORES = cpu_count()
 
-    hashes = await asyncio.gather(
-        hash256(password),
-        hash512(password)
-    )
+    hash_256 = hash256(password)
+    hash_512 = hash512(password)
+    len_password = len(password)
 
-    print("\t256 SHA: ", hashes[0].decode("utf-8") )
-    print("\t512 SHA: ", hashes[1].decode("utf-8"), "\n")
+    print("\t256 SHA: ", hash_256.decode("utf-8") )
+    print("\t512 SHA: ", hash_512.decode("utf-8"), "\n")
 
     layer = 1
-    futures = []
+    crackers( hash_256, hash_512, dict_array, layer, len_password)
 
-    with concurrent.futures.ProcessPoolExecutor(NUM_CORES) as executor:
-       
-            new_future = executor.submit(
-                crackers, # Function to perform
-                # v Arguments v
-                hashes[0],
-                hashes[1],
-                dict_array,
-                layer
-            )
-            futures.append(new_future)
-
-    concurrent.futures.wait(futures)
-
-    
-
-
-
-async def checker256(hashed_pwd_hex_256: bytes, hashed_pwd_hex_512: bytes, dict_array: list[str], layer:int):
+def checker256(hashed_pwd_hex_256: bytes, hashed_pwd_hex_512: bytes, dict_array: list[str], layer:int, len_password:int):
         time256 = time.time()
         guesses = 0
         for perm in permutations(dict_array, layer):
+            if (len(perm) > len_password):
+                continue
             guesses = guesses + 1
             temp = ''
             for i in range(len(perm)):
                 temp = temp + perm[i]
-            if await hash256(temp) == hashed_pwd_hex_256:
+            if hash256(temp) == hashed_pwd_hex_256:
                 print("Cracked SHA256: ", temp)
                 print("Time to crack: ", time.time() - time256, "\n")
                 return [True, guesses, layer]
-        await checker256(hashed_pwd_hex_256, hashed_pwd_hex_512, dict_array, layer + 1)
-        if layer > len(dict_array):
+        checker256(hashed_pwd_hex_256, hashed_pwd_hex_512, dict_array, layer + 1, len_password)
+        if layer > len_password:
             return [False]
 
-async def checker512(hashed_pwd_hex_256: bytes, hashed_pwd_hex_512: bytes, dict_array: list[str], layer:int):
+def checker512(hashed_pwd_hex_256: bytes, hashed_pwd_hex_512: bytes, dict_array: list[str], layer:int, len_password:int):
         time512 = time.time()
         guesses = 0
-        for perm in permutations(dict_array, layer):
+        for perm in  permutations(dict_array, layer):
+            if (len(perm) > len_password):
+                continue
             guesses = guesses + 1
             temp = ''
             for i in range(len(perm)):
                 temp = temp + perm[i]
-            if await hash512(temp) == hashed_pwd_hex_512:
+            if hash512(temp) == hashed_pwd_hex_512:
                 print("Cracked SHA512: ", temp)
                 print("Time to crack: ", time.time() - time512, "\n")
                 return [True, guesses, layer]
-        await checker512(hashed_pwd_hex_256, hashed_pwd_hex_512, dict_array, layer + 1)
-        if layer > len(dict_array):
+        checker512(hashed_pwd_hex_256, hashed_pwd_hex_512, dict_array, layer + 1, len_password)
+        if layer > len_password:
             return [False]
 
-def crackers(hashed_pwd_hex_256: bytes, hashed_pwd_hex_512: bytes, dict_array: list[str], layer:int):
-    if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and sys.platform.startswith('win'):
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+def crackers(hashed_pwd_hex_256: bytes, hashed_pwd_hex_512: bytes, dict_array, layer:int, len_password:int):
+    procs = []
+    proc256 = Process(target=checker256, args=(hashed_pwd_hex_256, hashed_pwd_hex_512, dict_array, layer, len_password))
+    proc512 = Process(target=checker512, args=(hashed_pwd_hex_256, hashed_pwd_hex_512, dict_array, layer, len_password))
+    procs.append(proc256)
+    procs.append(proc512)
+    proc256.start()
+    proc512.start()
 
-    results256 = asyncio.run( checker256(hashed_pwd_hex_256, hashed_pwd_hex_512, dict_array, layer) )
-    results512 = asyncio.run( checker512(hashed_pwd_hex_256, hashed_pwd_hex_512, dict_array, layer) )
+    for proc in procs:
+        proc.join()
 
-    if (not results256):
+    if (not proc256):
         print("No SHA256 Passwords Cracked")
-    if (not results512):
+    if (not proc512):
         print("No SHA512 Passwords Cracked")
 
 start = time.time()
@@ -111,7 +97,7 @@ if __name__ == '__main__':
     while (True):
         password = input("Enter password: ")
         while (password != 'q'):
-            asyncio.run(main())
+            main()
             password = input("Enter password: ")
         else:
             print("Elpased time: ", (time.time() - start), " seconds.")
